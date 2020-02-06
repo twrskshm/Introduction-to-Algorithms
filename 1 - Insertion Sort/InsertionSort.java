db2ac3e651e5d4d49fae780e6c9e85579aab05d0
t-saksham <57420341+t-saksham@users.noreply.github.com>
2020-02-06 18:55:34 -0500

    First commit
    First commit

---
+++ 1 - Insertion Sort/InsertionSort.java
@@ -0,0 +1,17 @@
+public class InsertionSort {
+    public static int[] insertion_sort(int[] input_array) {
+        for(int i = 1; i < input_array.length; i++) {
+            int j = i;
+
+            while(j > 0 && input_array[j] < input_array[j - 1]) {
+                int temp = input_array[j];
+                input_array[j] = input_array[j - 1];
+                input_array[j - 1] = temp;
+
+                j--;
+            }
+        }
+
+        return input_array;
+    }
+}
