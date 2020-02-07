bdf53b98346d3f2188b11c21267f73c0744883cc
t-saksham <57420341+t-saksham@users.noreply.github.com>
2020-02-06 19:53:40 -0500

    Variable name change
    Variable name change

--- 1 - Insertion Sort/InsertionSort.java
+++ 1 - Insertion Sort/InsertionSort.java
@@ -1,12 +1,12 @@
 public class InsertionSort {
-    public static void insertion_sort(int[] input_array) {
-        for(int i = 1; i < input_array.length; i++) {
+    public static void insertion_sort(int[] array) {
+        for(int i = 1; i < array.length; i++) {
             int j = i;
 
-            while(j > 0 && input_array[j] < input_array[j - 1]) {
-                int temp = input_array[j];
-                input_array[j] = input_array[j - 1];
-                input_array[j - 1] = temp;
+            while(j > 0 && array[j] < array[j - 1]) {
+                int temp = array[j];
+                array[j] = array[j - 1];
+                array[j - 1] = temp;
 
                 j--;
             }
