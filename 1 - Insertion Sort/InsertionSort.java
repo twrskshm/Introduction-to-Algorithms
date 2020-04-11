1230968c15eaece6c8349f1c66871c32ed60dab5
Saksham Tiwari <contact@tsaksham.com>
2020-04-10 22:23:55 -0400

    Update
    Update

--- 1 - Insertion Sort/InsertionSort.java
+++ 1 - Insertion Sort/InsertionSort.java
@@ -1,14 +1,17 @@
+/*
+Chapter 2: Getting Started
+2.1 Insertion sort
+*/
+
 public class InsertionSort {
-    public static void insertion_sort(int[] array) {
-        for(int i = 1; i < array.length; i++) {
+    void insertionSort(final int[] array) {
+        for (int i = 1; i < array.length; i++) {
             int j = i;
 
-            while(j > 0 && array[j] < array[j - 1]) {
-                int temp = array[j];
-                array[j] = array[j - 1];
-                array[j - 1] = temp;
-
-                j--;
+            while (j > 0 && array[j - 1] > array[j]) {
+                int swapValue = array[j - 1];
+                array[j - 1] = array[j];
+                array[j--] = swapValue;
             }
         }
     }
